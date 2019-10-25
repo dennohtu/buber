eea9529b8b417056002ee59e3fc1ea0e499c0c9c
dennohtu <dennismureithi10@gmail.com>
2019-10-25 17:14:37 +0300

    login signup complete

---
+++ app/src/main/java/com/gpridesolutions/buberrider/models/Rider.java
@@ -0,0 +1,28 @@
+package com.gpridesolutions.buberrider.models;
+
+public class Rider {
+    private String email, password, name, phone;
+
+    public Rider(String email, String password, String name, String phone){
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
